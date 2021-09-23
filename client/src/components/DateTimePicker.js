b0e799e16bba2de556ccccf55da77c4a5cce2041
ganciaux <ghislain.anciaux@live.fr>
2021-09-23 07:18:24 +0200

    Add datepickers

--- client/src/components/DateTimePicker.js
+++ client/src/components/DateTimePicker.js
@@ -1,42 +1,206 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useState, forwardRef } from 'react';
+import DatePicker, { CalendarContainer } from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { registerLocale, setDefaultLocale } from  "react-datepicker";
 import fr from 'date-fns/locale/fr';
+import getMonth from "date-fns/getMonth";
+import getYear from "date-fns/getYear";
+import range from "lodash/range";
 
 registerLocale('fr', fr)
 setDefaultLocale('fr');
 
-function DateDatePicker () {
+export function DateDatePicker () {
   const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button type="button" className="example-custom-input btn btn-primary" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
   return (
     <DatePicker
       selected={startDate}
       onChange={(date) => setStartDate(date)}
-      showTimeSelect
+      customInput={<ExampleCustomInput />}
       locale="fr"
-      timeFormat="HH:mm"
-      timeIntervals={15}
-      timeCaption="Heure"
       dateFormat="dd/MM/yyyy"
-      />
+      peekNextMonth
+      showMonthDropdown
+      showYearDropdown
+      dropdownMode="select">
+      </DatePicker>
     );
   };
 
-function DateTimePicker () {
+export function DateTimePicker (props) {
   const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button type="button" className="example-custom-input btn btn-primary" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+
+  const handleChange = date => {
+        props.onchange(date);
+    }
+  
   return (
     <DatePicker
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
+      onChange={(date) => { setStartDate(date); handleChange(date) }}
+      customInput={<ExampleCustomInput />}
       showTimeSelect
       locale="fr"
       timeFormat="HH:mm"
       timeIntervals={15}
       timeCaption="Heure"
-      dateFormat="dd/MM/yyyy - hh:mm"
-      />
+      dateFormat="dd/MM/yyyy - HH:mm"
+      peekNextMonth
+      showMonthDropdown
+      showYearDropdown
+      dropdownMode="select"
+    ></DatePicker>
     );
   };
 
-export default DateTimePicker;
+  export function DatePickerCustom() {
+  const [startDate, setStartDate] = useState(new Date());
+  const MyContainer = ({ className, children }) => {
+    return (
+      <div style={{ padding: "16px", background: "#216ba5", color: "#fff" }}>
+        <CalendarContainer className={className}>
+          <div style={{ background: "#f0f0f0" }}>
+            What is your favorite day?
+          </div>
+          <div style={{ position: "relative" }}>{children}</div>
+        </CalendarContainer>
+      </div>
+    );
+  };
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      calendarContainer={MyContainer}
+      isClearable
+    />
+  );
+};
+
+export function DatePickerCustomInput() {
+  const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button type="button" className="example-custom-input btn btn-primary" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      customInput={<ExampleCustomInput />}
+      showTimeSelect
+      locale="fr"
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      timeCaption="Heure"
+      dateFormat="dd/MM/yyyy - HH:mm"></DatePicker>
+  );
+};
+
+export function DatePickerHeader() {
+  const [startDate, setStartDate] = useState(new Date());
+  const years = range(1990, getYear(new Date()) + 1, 1);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return (
+    <DatePicker
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div
+          style={{
+            margin: 10,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <button type="button" onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            {"<"}
+          </button>
+          <select
+            value={getYear(date)}
+            onChange={({ target: { value } }) => changeYear(value)}
+          >
+            {years.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <select
+            value={months[getMonth(date)]}
+            onChange={({ target: { value } }) =>
+              changeMonth(months.indexOf(value))
+            }
+          >
+            {months.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <button type="button" onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            {">"}
+          </button>
+        </div>
+      )}
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+    />
+  );
+};
+
+export function DatePickerRange() {
+  const [startDate, setStartDate] = useState(new Date("2014/02/08"));
+  const [endDate, setEndDate] = useState(new Date("2014/02/10"));
+  return (
+    <>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+      />
+    </>
+  );
+};
--- client/src/pages/TestApp.js
+++ client/src/pages/TestApp.js
@@ -1,112 +1,36 @@
 import React, { Fragment, useState, useEffect, useReducer } from 'react';
-import DateTimePicker from '../components/DateTimePicker';
+import { DateDatePicker, DateTimePicker, DatePickerCustom, DatePickerCustomInput, DatePickerHeader, DatePickerRange } from '../components/DateTimePicker';
 import axios from 'axios';
 
-const dataFetchReducer = (state, action) => {
-  switch (action.type) {
-    case 'FETCH_INIT':
-      return {
-        ...state,
-        isLoading: true,
-        isError: false
-      };
-    case 'FETCH_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case 'FETCH_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    default:
-      throw new Error();
-  }
-};
-
-const useDataApiReducer = (initialUrl, initialData) => {
-  const [url, setUrl] = useState(initialUrl);
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    isLoading: false,
-    isError: false,
-    data: initialData,
-  });
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      dispatch({ type: 'FETCH_INIT' });
-      try {
-        const result = await axios(url);
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data.data });
-      } catch (error) {
-        dispatch({ type: 'FETCH_FAILURE' });
-      }
-    };
- 
-    fetchData();
-  }, [url]);
- 
-  return [state, setUrl];
-}
-
-const useDataApi = (initialUrl, initialData) => {
-  const [data, setData] = useState(initialData);
-  const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      try {
-        const result = await axios(url);
- 
-        setData(result.data);
-      } catch (error) {
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
- 
-    fetchData();
-  }, [url]);
- 
-  return [{ data, isLoading, isError }, setUrl];
-}
-
 function TestApp() {
-  const [query, setQuery] = useState('redux');
-  const [{ data, isLoading, isError }, doFetch] = useDataApiReducer('http://localhost:5000/api/clients/?query=redux', { data: []});
- 
   return (
-    <Fragment>
-      <DateTimePicker />
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button type="button" onClick={() => doFetch(`http://localhost:5000/api/clients/?query=${query}`)}>
-        Search
-      </button>
-      {isError && <div>Something went wrong ...</div>}
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-      <ul>
-          {data.data.map(item => (
-            <li key={item._id}>
-              <a href={`/clients/${item.slug}`}>{item.name} - {item.firstname}</a>
-            </li>
-          ))}
-      </ul>
-      )}
-    </Fragment>
+    <div className="container" >
+
+      <form className="form-horizontal">
+        <fieldset>
+            <legend>Test</legend>
+            <div className="form-group col-6 col-sm-4 col-md-3">
+            <DateTimePicker onchange={(e) => { }}></DateTimePicker>
+          </div>
+          <div className="form-group col-2">
+            <DateDatePicker onchange={(e) => { }}></DateDatePicker>
+          </div>
+          <div className="form-group col-2">
+            <DatePickerCustom></DatePickerCustom>
+          </div>
+          <div className="form-group col-2">
+            <DatePickerCustomInput></DatePickerCustomInput>
+          </div>
+          <div className="form-group col-2">
+            <DatePickerHeader></DatePickerHeader>
+          </div>
+          <div className="form-group col-2">
+            <DatePickerRange></DatePickerRange>
+          </div>
+          
+            </fieldset>
+          </form>
+    </div>
   );
 }
  
